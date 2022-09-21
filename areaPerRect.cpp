c9b740efbe977a79f9d90d82fc378cc2d40e02d1
Jessah Tumbaga <jessah.tumbaga@stu.ocsb.ca>
2022-09-21 06:40:19 +0000

    Added areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) Jessah All rights reserved.
+//
+// Name Jessah
+// 09/19/2022
+// This program performs basic math
+
+#include <iostream>
+
+int main() {
+    std::cout << "If the rectangle has the dimensions:" << std::endl;
+    std::cout << "5cm x 3cm " << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is: " << (5 * 3) << "cm^2" << std::endl;
+    std::cout << "Perimeter is: " << (2 * (5 + 3)) << "cm" << std::endl;
+}
---
+++ basicMath.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) Jessah All rights reserved.
+//
+// Name Jessah
+// 09/19/2022
+// This program performs basic math
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "9+2=" << (9 + 2) << std::endl;
+    std::cout << "7-2=" << (7 - 2) << std::endl;
+    std::cout << "5/2=" << (4 / 2) << std::endl;
+    std::cout << "4+4/2=" << (4 + 4 / 2) << std::endl;
+    std::cout << "3+4*2=" << (3 + 4 * 2) << std::endl;
+    std::cout << "5+2^3=" << (5 + 2 ^ 3) << std::endl;
+    std::cout << "5+pow(2,3)=" << (5 + pow(2, 3)) << std::endl;
+    std::cout << sqrt(25) << std::endl;
+}
+
